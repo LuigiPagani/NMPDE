25651b959ac575afd7d58000d5b1c2ef1f3bc413
Luigi Pagani <luigi2.pagani@mailpolimi.it>
2024-03-29 22:34:01 +0000

    commit

---
+++ build/CMakeFiles/domain_decomposition.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/domain_decomposition.dir/src_domain_decomposition/Poisson2D.cpp.o"
+  "CMakeFiles/domain_decomposition.dir/src_domain_decomposition/Poisson2D.cpp.o.d"
+  "CMakeFiles/domain_decomposition.dir/src_domain_decomposition/domain_decomposition_main.cpp.o"
+  "CMakeFiles/domain_decomposition.dir/src_domain_decomposition/domain_decomposition_main.cpp.o.d"
+  "domain_decomposition"
+  "domain_decomposition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/domain_decomposition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/elliptic_1D.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/luigi/NMPDE/src_elliptic_1D/Elliptic.cpp" "CMakeFiles/elliptic_1D.dir/src_elliptic_1D/Elliptic.cpp.o" "gcc" "CMakeFiles/elliptic_1D.dir/src_elliptic_1D/Elliptic.cpp.o.d"
+  "/home/luigi/NMPDE/src_elliptic_1D/elliptic_main.cpp" "CMakeFiles/elliptic_1D.dir/src_elliptic_1D/elliptic_main.cpp.o" "gcc" "CMakeFiles/elliptic_1D.dir/src_elliptic_1D/elliptic_main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
